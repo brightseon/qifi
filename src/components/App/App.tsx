035fa1d722497fef02733666281e80312f949a70
brightseon <public.seon@gmail.com>
2023-02-12 16:15:20 +0900

    feat: css module 설정

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,7 @@
-const App = () => <div>App</div>;
+import styles from './styles.css';
+
+console.log(styles);
+
+const App = () => <div className={styles.app}>App</div>;
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const MODE = process.env.MODE;
+const IS_DEV = MODE === 'development';
 const PORT = process.env.PORT || 3000;
 
 module.exports = {
@@ -24,10 +26,28 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: [{ loader: 'babel-loader' }, { loader: 'ts-loader' }],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: IS_DEV ? 'style-loader' : MiniCssExtractPlugin.loader },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: IS_DEV ? '[local]-[hash:base64:5]' : '[hash:base64]'
+                            }
+                        }
+                    }
+                ],
+                exclude: /node_modules/
             }
         ]
     },
-    plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+    plugins: [
+        new HtmlWebpackPlugin({ template: './public/index.html' }),
+        new MiniCssExtractPlugin({ filename: 'bundle.css' })
+    ],
     devServer: {
         historyApiFallback: true,
         port: PORT,
