706e565acbe062411cfd2e1c859075d0f54fde2b
brightseon <public.seon@gmail.com>
2023-02-12 17:39:18 +0900

    refactor: 임시 파일 삭제

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,3 @@
-import styles from './styles.scss';
-import test from '../../../images/test.svg';
-
-console.log(styles);
-
-const App = () => (
-    <div className={styles.app}>
-        <h1 className={styles.heading}>App</h1>
-        <span className={styles.hello}>Hello!</span>
-        <img src={test} />
-    </div>
-);
+const App = () => <div>App</div>;
 
 export default App;
