033f9a155eb21f0c7c6aedc1369280ad5d1edfee
brightseon <public.seon@gmail.com>
2023-02-12 17:43:20 +0900

    feat: react-router-dom 적용

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,13 @@
-const App = () => <div>App</div>;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { PATH } from '../../constants/path';
+import Home from '../../pages/Home';
+
+const App = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path={PATH.home} element={<Home />} />
+        </Routes>
+    </BrowserRouter>
+);
 
 export default App;
