36cf699188eff9a2dbefa94135991a71c151b228
brightseon <public.seon@gmail.com>
2023-02-12 16:32:01 +0900

    feat: clean-webpack-plugin 추가

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const MODE = process.env.MODE;
 const IS_DEV = MODE === 'development';
@@ -47,7 +48,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({ template: './public/index.html' }),
-        new MiniCssExtractPlugin({ filename: 'bundle.css' })
+        new MiniCssExtractPlugin({ filename: 'bundle.css' }),
+        new CleanWebpackPlugin()
     ],
     devServer: {
         historyApiFallback: true,
