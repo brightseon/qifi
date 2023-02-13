6ba6c6d320fbb07a62b53e80f9b822f58fc31b2c
brightseon <public.seon@gmail.com>
2023-02-13 23:31:44 +0900

    feat: typescript 절대 경로 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import { createRoot } from 'react-dom/client';
-import App from './components/App';
+import App from '@components/App';
 
 createRoot(document.getElementById('app-root')).render(<App />);
