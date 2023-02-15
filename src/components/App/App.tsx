5f4d54033743e58c0c0693f397d060993b1d07b5
brightseon <public.seon@gmail.com>
2023-02-15 23:08:11 +0900

    feat: Route 추가

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { PATH } from '@constants/path';
 import Home from '@pages/Home';
+import New from '@pages/New';
 
 const App = () => (
     <BrowserRouter>
         <Routes>
             <Route path={PATH.home} element={<Home />} />
+            <Route path={PATH.new} element={<New />} />
         </Routes>
     </BrowserRouter>
 );
