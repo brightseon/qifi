d114a422fcbfceb34d247fa49157e7e24e5b8a4a
brightseon <public.seon@gmail.com>
2023-02-15 23:40:38 +0900

    feat: 이름 입력 추가

--- src/pages/New/New.tsx
+++ src/pages/New/New.tsx
@@ -1,30 +1,38 @@
-const New = () => (
-    <div>
+import { ChangeEvent, useState } from 'react';
+
+const New = () => {
+    const [name, setName] = useState('');
+
+    const changeName = (e: ChangeEvent<HTMLInputElement>) => setName(e.target.value);
+
+    return (
         <div>
-            <label>이름</label>
-            <input name="name" />
-        </div>
-        <div>
-            <label>비밀번호</label>
-            <input type={'password'} />
-        </div>
-        <div>
-            <label>보안 프로토콜</label>
             <div>
-                <input id="none" type={'radio'} name="None" />
-                <label htmlFor="none">None</label>
-                <input id="wpa" type={'radio'} name="WPA/WPA2" />
-                <label htmlFor="wpa">WPA/WAP2</label>
-                <input id="wep" type={'radio'} name="WEP" />
-                <label htmlFor="wep">WEP</label>
+                <label>이름</label>
+                <input name="name" value={name} onChange={changeName} />
             </div>
+            <div>
+                <label>비밀번호</label>
+                <input type={'password'} />
+            </div>
+            <div>
+                <label>보안 프로토콜</label>
+                <div>
+                    <input id="none" type={'radio'} name="None" />
+                    <label htmlFor="none">None</label>
+                    <input id="wpa" type={'radio'} name="WPA/WPA2" />
+                    <label htmlFor="wpa">WPA/WAP2</label>
+                    <input id="wep" type={'radio'} name="WEP" />
+                    <label htmlFor="wep">WEP</label>
+                </div>
+            </div>
+            <div>
+                <label>설명</label>
+                <input type={'text'} name="description" />
+            </div>
+            <button type="button">생성하기</button>
         </div>
-        <div>
-            <label>설명</label>
-            <input type={'text'} name="description" />
-        </div>
-        <button type="button">생성하기</button>
-    </div>
-);
+    );
+};
 
 export default New;
