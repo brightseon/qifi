a8cd814a6c569394fbe4fb34843a0d29336964ba
brightseon <public.seon@gmail.com>
2023-03-19 21:25:41 +0900

    refactor: Description 적용

--- src/pages/New/New.tsx
+++ src/pages/New/New.tsx
@@ -1,9 +1,9 @@
 import useQRForm from '@hooks/useQRForm';
-import Input from '@components/Input';
 import Button from '@components/Button';
 import Ssid from '@components/Ssid';
 import Password from '@components/Password';
 import Protocol from '@components/Protocol';
+import Description from '@components/Description';
 
 const New = () => {
     const {
@@ -24,10 +24,7 @@ const New = () => {
             <Ssid ssid={ssid} changeSSID={changeSSID} />
             <Password password={password} changePassword={changePassword} />
             <Protocol protocol={protocol} changeProtocol={changeProtocol} />
-            <div>
-                <label>설명</label>
-                <Input value={description} onChange={changeDescription} />
-            </div>
+            <Description description={description} changeDescription={changeDescription} />
             <Button onClick={createQR}>생성하기</Button>
             {qr && (
                 <>
